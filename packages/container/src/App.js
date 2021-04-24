2a39d4b1d169ebcd49d97df1520a96c49699f24d
Eric Humphries <ezhumphries@gmail.com>
2021-04-24 10:01:38 -0700

    lame duck commit

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <Fragment>
-      <h1>Hi there! Buuddy! Ya Boob!</h1>
+      <h1>Hi there! Buuddy! Ya!Ya!</h1>
       <hr></hr>
       <MarketingApp />
     </Fragment>
